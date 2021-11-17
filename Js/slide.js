79721f36a5ac4b54c5f58856e9548f52bfcdd013
MatiTorres18133 <84334646+MatiTorres18133@users.noreply.github.com>
2021-11-17 14:21:42 -0300

    first

---
+++ Js/slide.js
@@ -0,0 +1,656 @@
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("animado");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover 1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover2");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover2 2s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover3");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover3 1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover4");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover4 1s ease-out"
+    }
+
+})
+
+
+
+
+
+/*----------------------------MOVER BOTONES------------ */
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverBUT1");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverBTN1 1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverBUT2");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverBTN2 1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverBUT3");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverBTN3 1s ease-out"
+    }
+
+})
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverBUT4");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover5 1s ease-out"
+    }
+
+})
+
+
+
+/*------------------MOVER DIVS--------------- */
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverTIT1");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverT1 1s ease-out"
+    }
+
+})
+
+
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverTIT3");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverT3 1s ease-out"
+    }
+
+})
+
+
+
+/*--------------TITULO------- */
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("NT");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverNT 1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("TXTGL");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverGL 1s ease-out"
+    }
+
+})
+
+
+
+/*---------------------MOVER IMG------------------ */
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL1");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL1  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL2");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL2  1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL3");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL3  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL4");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL4  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL5");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL5  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL6");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL6  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL7");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL7  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL8");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL8  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL9");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL9  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL10");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL10  1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL11");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL11  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("IMGGL12");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMGGL12  1s ease-out"
+    }
+
+})
+
+
+
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverCE1");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverCE  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverCE2");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverCE  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverCE3");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverCE2  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverCE4");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverCE2  1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverGL");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverGLP 1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover-boton-galeria-img");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverBTN-IMG 1s ease-out"
+    }
+
+})
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("SENDING");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverSEND 1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("Form");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverFORM 1s ease-out"
+    }
+
+})
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverTITLE2");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverT2 1s ease-out"
+    }
+
+})
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverTIT2");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverT2 1s ease-out"
+    }
+
+})
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("moverTXT-FORM");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="movertext-form 1s ease-out"
+    }
+
+})
+
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("img-glamping");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverIMG-glamp 1s ease-out"
+    }
+
+})
+
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover-txt-glamp");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverTXT-glamp 1s ease-out"
+    }
+
+})
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover-descp-glamp");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="moverDESCP-glamp 1.5s ease-out"
+    }
+
+})
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover-algunos-t");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover-Img-slider 1.5s ease-out"
+    }
+
+})
+
+
+
+
+
+
+window.addEventListener("scroll", function(){
+
+    let animacion =this.document.getElementById("mover-txt-algunos");
+    let posticionObj1= animacion.getBoundingClientRect().top;
+    console.log(posticionObj1);
+    let tamañoDePantalla= window.innerHeight;
+
+
+    if(posticionObj1 < tamañoDePantalla){
+        animacion.style.animation="mover-algunos-slider 1.5s ease-out"
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
